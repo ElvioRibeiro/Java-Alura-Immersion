e79bcbde3beb6ec04fff255245a7d47583bf3c6e
ElvioRibeiro <83252894+ElvioRibeiro@users.noreply.github.com>
2023-03-24 22:10:02 -0300

    Request IMDB list

---
+++ Request-api-imdb/src/App.java
@@ -0,0 +1,18 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String url = "";
+        HttpClient client = new HttpClient.newHttpClient();
+        HttpRequest reqAPI = HttpRequest.newBuilder(URI.create(url)).GET().build();
+        HttpResponse <String> response = client.send(reqAPI, BodyHandlers.String());
+        String json = response.body();
+        System.out.println(json);
+
+    }
+}
