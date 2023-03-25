a2142399a86a6c09a4ccfb28b61a219ab343ce42
ElvioRibeiro <83252894+ElvioRibeiro@users.noreply.github.com>
2023-03-24 22:27:41 -0300

    Capturing json

--- Request-api-imdb/src/App.java
+++ Request-api-imdb/src/App.java
@@ -7,10 +7,10 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String url = "";
-        HttpClient client = new HttpClient.newHttpClient();
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_12345678";
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest reqAPI = HttpRequest.newBuilder(URI.create(url)).GET().build();
-        HttpResponse <String> response = client.send(reqAPI, BodyHandlers.String());
+        HttpResponse <String> response = client.send(reqAPI, BodyHandlers.ofString());
         String json = response.body();
         System.out.println(json);
 
